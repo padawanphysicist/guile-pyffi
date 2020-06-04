a249b8fa84eff1a1da6b8142c35268ef4180bf65
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2020-06-04 10:40:48 -0300

    Add finalization definition

--- pyffi/pyffi.scm
+++ pyffi/pyffi.scm
@@ -1,5 +1,5 @@
 (define-module (pyffi)
-  #:export (py-initialize py-initialized? *py-main-module* *py-main-module-dict*))
+  #:export (py-initialize py-initialized? py-finalize *py-main-module* *py-main-module-dict*))
 
 (define *py-main-module* (make-parameter #f))
 (define *py-main-module-dict* (make-parameter #f))
@@ -21,3 +21,14 @@
       (*py-main-module-dict* (PyModule_GetDict (*py-main-module*)))
       (Py_INCREF (*py-main-module-dict*))
       (display "Python interpreter initialized.\n"))))
+
+(define (py-finalize)
+  (if (not (py-initialized?))
+    (display "Python interpreter not initialized. Doing nothing.\n")
+    (begin
+      (Py_DECREF (*py-main-module-dict*))
+      (*py-main-module* #f)
+      (*py-main-module-dict* #f)
+      (if (= 0 (Py_FinalizeEx))
+	(display "Python interpreter finalized with success.\n")
+	(display "Python interpreter finalized with errors.\n")))))
